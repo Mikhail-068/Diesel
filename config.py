fb5dd308d1a90c7490f3f24d32e8377046807bf4
Mikhail Titov <89531276499nokia@gmail.com>
2021-11-04 14:09:22 +0300

    password

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ surnames = 'Титов,Алексеев,Савельев,Черкасов,Юрь
 
 m = {'Field': 'id', 'Type': 'int', 'Null': 'NO', 'Key': 'PRI', 'Extra': 'auto_increment'}
 
-
 # key_ = []
 # for i in m.keys():
 #     key_.append(i)
@@ -22,4 +21,6 @@ m = {'Field': 'id', 'Type': 'int', 'Null': 'NO', 'Key': 'PRI', 'Extra': 'auto_in
 #
 # test()
 # print(a)
+en1 = 'C8Uf!en1=C8Uf!en1=C8Uf!'
 
+Git = '(Mikhail-068   PoloSedan_1986)'
