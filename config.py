4c7e859706c770cc2b1d053413a826897852e044
Mike <89531276499nokia@gmail.com>
2021-11-04 15:25:30 +0300

    Тестируем показ таблицы в config

--- config.py
+++ config.py
@@ -2,25 +2,23 @@ host = 'localhost'
 user = 'root'
 password = 'Cdtnjxrf86'
 name = 'diesel'
+import datetime
 
-surnames = 'Титов,Алексеев,Савельев,Черкасов,Юрьев,Сажнев,Авилочкин,Дильдин,Усачев,Кизиль,Бирюков,Жирнов,' \
-           'Пустынников,Назин'.split(',')
+# surnames = 'Титов,Алексеев,Савельев,Черкасов,Юрьев,Сажнев,Авилочкин,Дильдин,Усачев,Кизиль,Бирюков,Жирнов,' \
+#            'Пустынников,Назин'.split(',')
 
-m = {'Field': 'id', 'Type': 'int', 'Null': 'NO', 'Key': 'PRI', 'Extra': 'auto_increment'}
+a = {'id': 13, 'date_': datetime.date(2021, 10, 1), 'liters': 30, 'id_driver': 10}
 
-# key_ = []
-# for i in m.keys():
-#     key_.append(i)
-# print(key_[:2])
+def razbor(arg):
+    keys = [i for i in arg.keys()]
+    values = [str(i) for i in arg.values()]
+    str_keys = ' | '.join(keys)
+    print(str_keys)
+    print('-' * len(str_keys))
+    print(' | '.join(values))
 
-#
-# def test():
-#     global a
-#     a = 'Mike'
-#     # print(a)
-#
-# test()
-# print(a)
-en1 = 'C8Uf!en1=C8Uf!en1=C8Uf!'
+    # print(keys)
+    # print('-'*35)
+    # print(values)
 
-Git = '(Mikhail-068   PoloSedan_1986)'
+razbor(a)
